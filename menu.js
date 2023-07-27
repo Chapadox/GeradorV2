faa744ba314ab4de8e1fcb28556735ff3e7d2834
Chapadox <ffcxcfsantos2@gmail.com>
2023-07-27 15:19:54 -0300

    Finish Project

--- menu.js
+++ menu.js
@@ -2,8 +2,21 @@ import chalk from 'chalk';
 import select from '@inquirer/select';
 import index from './index.js';
 
+console.clear()
+const log = chalk.bold(chalk.blueBright(`
+          |---------------| 
+            CORE RELATOS ©
+          |---------------|
+
+1 - Crie Relatos Com ${chalk.red('Apenas Alguns clicks')}
+2 - Interface de ${chalk.red('Facil Utilização.')}
+3 - Sistema ${chalk.red('exclusivo')}
+
+`))
 
 async function menu() {
+  console.clear();
+  console.log(log)
   const perg = await select({message: `Selecione uma Opção: `, choices: [{value: '* - iniciar Atendimento'}, {value: '* - Sair'}]})
     if(perg == '* - iniciar Atendimento') {
     await index()
